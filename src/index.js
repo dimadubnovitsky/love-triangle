38543b8881cdbdb69c1c0247f3ab42faa4c4c233
Dima Dubnovitsky <dimadubnovitsky@gmail.com>
2019-03-24 17:31:31 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -4,4 +4,19 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  // preferences = [2, 3, 1]
+  let count = 0;
+
+  for (let i = 0; i < preferences.length; i++) {
+    let x = preferences[i] - 1; // x = 1
+    if (x === i) continue;
+    let y = preferences[x] - 1; // y = 2
+    if (x === y) continue;
+    let z = preferences[y] - 1; // z = 0 
+    if (z === y) continue;
+    if (z === i) {
+      count++;
+    }
+  }
+  return count / 3;
 };
